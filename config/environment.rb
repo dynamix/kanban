c015b2c096b26b6d431418d61f7e690986cbdb3f
thomashille <thomas.hille@madvertise.de>
2009-10-06 16:24:38 +0200

    removed pagination

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   
-  config.gem 'will_paginate'
+  #config.gem 'will_paginate'
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
