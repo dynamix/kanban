cec958b198a0e16895fdc59637009ceeab70fed4
thomashille <thomas.hille@madvertise.de>
2010-04-14 18:27:55 +0200

    excluded weekdays from statistics

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -7,7 +7,12 @@ class Statistic < ActiveRecord::Base
   
   def calc_duration
     if entry_time && leave_time
+      number_of_days = (self.leave_time - self.entry_time) / (3600 * 24)
+      number_of_days = (number_of_days - 0.5).round
+      week_day_of_leave = self.leave_time.wday
       self.duration = self.leave_time - self.entry_time
+      self.duration -= ((number_of_days / 7) * 2 * 3600 * 24)
+      self.duration -= 2 if (number_of_days % 7) - 2 >= week_day_of_leave
     end
   end
 end
